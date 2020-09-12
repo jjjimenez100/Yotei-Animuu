2cbb35c40a443670a3c9ccf0f56007e48a5988e5
jjjimenez100 <jimenez.johnjoshua.jjj@gmail.com>
2020-09-13 01:58:00 +0800

    chore(persistence): force git change for lerna

--- packages/persistence/index.js
+++ packages/persistence/index.js
@@ -1,8 +1,6 @@
 const Repository = require('./lib/Repository');
 const ScheduledDownloadEntity = require('./lib/ScheduledDownloadEntity');
 
-// TODO: CLI app and integrations
-
 module.exports = {
   Repository,
   ScheduledDownloadEntity,
